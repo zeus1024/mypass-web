23ab1b2d19ad7bc25e432b66981590aa9f06f7ea
xjchen <xjchen1024@foxmail.com>
2023-07-11 19:03:04 +0800

    production

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: "docs",
+  publicPath: process.env.NODE_ENV === "production" ? "/mypass-web/" : "/",
 });
